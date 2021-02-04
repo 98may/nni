9904a0cc082d39332aa115499308c5a866036b16
AyanMao <98may@zju.edu.cn>
2021-02-05 03:24:59 +0800

    init dngo_darts' tuner+trial, ok for trials for the first geno batch

---
+++ examples/trials/may/trial.py
@@ -0,0 +1,43 @@
+import logging
+
+import numpy as np
+import torch
+
+from dngo_darts import query
+
+import nni
+
+_logger = logging.getLogger('May_DNGO')
+_logger.setLevel(logging.INFO)
+
+
+
+
+
+def main(params):
+    """
+    Main program:
+      - Build network
+      - Prepare dataset ????
+      - Train the model
+      - Report accuracy to tuner
+    """
+    # val_acc, test_acc = query(count, args.seed, params, args.inner_epochs)
+    val_acc, test_acc = query(981230, 3, params, 4)
+    _logger.info('Final accuracy reported: %s, %s', val_acc, test_acc)
+
+    accuracy = [val_acc, test_acc]
+
+    # send final accuracy to NNI tuner and web UI
+    _logger.info('Final accuracy reported: %s', accuracy)
+    nni.report_final_result(accuracy)
+    # _logger.info('Final accuracy reported: %s', accuracy)
+
+
+if __name__ == '__main__':
+
+    tuned_params = nni.get_next_parameter()
+    main(tuned_params)
+
+    # params.update(tuned_params)
+    # main(params)
